1ccda21ce1e4d00dc36d68ef5c43ff0ea64f20db
Expired <imoses2@hotmail.com>
2022-05-03 12:48:47 -0700

    added winston logger

--- server.js
+++ server.js
@@ -4,9 +4,26 @@ const path = require('path');
 const helpers = require('./utils/helpers');
 const mongoose = require('./config/connection');
 const session = require('express-session');
+const winston = require('winston');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//logging
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    //
+    // - Write all logs with importance level of `error` or less to `error.log`
+    // - Write all logs with importance level of `info` or less to `combined.log`
+    //
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
+
 
 //socket.io stuff
 const server = require('http').createServer(app);
@@ -43,4 +60,5 @@ app.use(routes);
 
 server.listen(PORT, () => { 
   console.log(`Server running on port ${PORT}`); 
+	logger.info(`Server running on port ${PORT}`); 
 });
