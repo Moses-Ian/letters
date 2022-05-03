daad7e87f8b74df89ca5d2c5863a19ea1769b485
Expired <imoses2@hotmail.com>
2022-05-03 10:36:01 -0700

    created chat app as proof of concept

---
+++ public/assets/js/socket.js
@@ -0,0 +1,50 @@
+const nameButton = document.getElementById('name-button');
+const joinRoomButton = document.getElementById('room-button');
+const messageInput = document.getElementById('message-input');
+const nameInput = document.getElementById('name-input');
+const roomInput = document.getElementById('room-input');
+const form = document.getElementById('form');
+
+let name = 'Guest';
+
+const socket = io();	//front is same domain as server
+socket.on('connect', () => {
+	displayMessage(`You connected with id: ${socket.id}`);
+	socket.emit('custom-event', 10, 'Hi', { a: 'a'});
+});
+
+socket.on('receive-message', message => {
+	displayMessage(message);
+});
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	if (messageInput.value === '') return;
+
+	const message = `${name}: ${messageInput.value}`;
+	const room = roomInput.value;
+	
+	displayMessage(message);
+	socket.emit('send-message', message, room);
+	
+	messageInput.value = '';
+});
+
+joinRoomButton.addEventListener('click', () => {
+	const room = roomInput.value;
+	socket.emit('join-room', room, message => {
+		displayMessage(message);	//this is a callback function. it will be called by the server in its .on() furction
+	});
+});
+
+nameButton.addEventListener('click', () => {
+	name = nameInput.value;
+});
+
+function displayMessage(message) {
+	const div = document.createElement('div');
+	div.textContent = message;
+	document.getElementById('message-container').append(div);
+}
+
+console.log('socket.js running');
--- server.js
+++ server.js
@@ -8,6 +8,24 @@ const session = require('express-session');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//socket.io stuff
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+io.on('connection', socket => {
+	console.log(socket.id);
+	socket.on('send-message', (message, room) => {
+		if (room === '')
+			socket.broadcast.emit('receive-message', message);
+		else
+			socket.to(room).emit('receive-message', message);
+	});
+	socket.on('join-room', (room, callback) => {
+		socket.join(room);
+		callback(`Joined ${room}`);
+	});
+});
+
 const sess = {
   secret: 'Super secret secret',
   cookie: {},
@@ -23,6 +41,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-app.listen(PORT, () => { 
+server.listen(PORT, () => { 
   console.log(`Server running on port ${PORT}`); 
 });
