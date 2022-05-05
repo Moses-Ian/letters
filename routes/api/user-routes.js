eeaa1dd56cdbcb581697e154b95c8f7abec48f94
Expired <imoses2@hotmail.com>
2022-05-05 08:16:08 -0700

    fixed filename capitalization

---
+++ routes/api/user-routes.js
@@ -0,0 +1,34 @@
+const router = require('express').Router();
+
+const {
+  getAllUser,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+	login,
+	logout
+} = require('../../controllers/user-controller');
+
+// /api/user
+router
+  .route('/')
+  .get(getAllUser)
+  .post(createUser);
+
+// /api/user/:id
+router
+  .route('/:id')
+  .get(getUserById)
+  .put(updateUser)
+  .delete(deleteUser);
+
+router
+	.route('/login')
+	.post(login);
+	
+router
+	.route('/logout')
+	.post(logout)
+
+module.exports = router;
