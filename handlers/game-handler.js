28184a6abbd5285aa1a5284260a99a7f77a6e947
Expired <imoses2@hotmail.com>
2022-05-06 17:00:05 -0700

    created public multiplayer version of the game

--- handlers/game-handler.js
+++ handlers/game-handler.js
@@ -1,16 +1,102 @@
+//variables
+//==================================
+let io;
 
-const registerGameHandler = (io, socket) => {
-	console.log(socket.id);
-	socket.on('send-message', (message, room) => {
-		if (room === '')
-			socket.broadcast.emit('receive-message', message);
-		else
-			socket.to(room).emit('receive-message', message);
-	});
-	socket.on('join-room', (room, callback) => {
-		socket.join(room);
-		callback(`Joined ${room}`);
-	});
+const vowels = ['a','e','i','o','u'];
+const consonants = ['d','h','t','n','s','p','y','f','g','c','r','l','q','j','k','x','b','m','w','v','z'];
+const weights =    [ 4 , 8 , 12, 16, 20, 23, 26, 29, 31, 33, 35, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46];
+
+let letters = [];
+let vowelCount = 0;
+let consonantCount = 0;
+let words = [];
+
+//functions
+//====================================
+addVowel = () => {
+	if (vowelCount == 5)
+		return;
+	if (letters.length == 9)
+		return;
+	let vowel = vowels[Math.floor(Math.random() * 5)];
+	let index = letters.length;
+	letters.push(vowel);	
+	vowelCount++;
+	io.emit('add-letter', vowel, index);
+};
+
+addConsonant = () => {
+	if (consonantCount == 6)
+		return;
+	if (letters.length == 9)
+		return;
+	let random = Math.floor(Math.random() * weights[20]);
+	let consonant;
+	for (let i=0; i<21; i++) {
+		if (weights[i] > random) {
+			consonant = consonants[i]
+			break;
+		}
+	}
+	let index = letters.length;
+	letters.push(consonant);
+	consonantCount++;
+	io.emit('add-letter', consonant, index);
+};
+
+submitWord = word => {
+	const score = scoreWord(word);
+	words.push({ word, score });
+	io.emit('append-word', word, score);
 }
 
+scoreWord = word => {
+	let checklist = new Array(word.length);
+	checklist.fill(false);
+	
+	//make sure every letter in the word is in letters
+	for(let i=0; i<letters.length; i++)
+		for(let j=0; j<word.length; j++)
+			if (letters[i] === word[j] && !checklist[j]) {
+				checklist[j] = true;
+				break;
+			}
+	for(let j=0; j<checklist.length; j++)
+		if (!checklist[j])
+			return 0;
+	
+	//make sure it's in the dictionary
+	if (!inDictionary(word))
+		return 0;
+	
+	return word.length;
+}
+
+inDictionary = word => {
+	//do something
+	return true;
+}
+
+restartLetters = () => {
+	letters = [];
+	vowelCount = 0;
+	consonantCount = 0;
+	words = [];
+	io.emit('clear-letters');
+};
+
+//listeners
+//=====================================
+const registerGameHandler = (newio, socket) => {
+	console.log(socket.id);
+	io = newio;
+	socket.on('add-vowel', addVowel);
+	socket.on('add-consonant', addConsonant);
+	socket.on('submit-word', submitWord);
+	socket.on('restart-letters', restartLetters);
+	socket.on('game-state', cb => cb(letters, words));
+}
+
+//export
+//=====================================
 module.exports = registerGameHandler;
---
+++ public/assets/js/multiplayer.js
@@ -0,0 +1,76 @@
+//variables
+//==================================
+let letterElArr = [];
+for (let i=0; i<9; i++)
+	letterElArr.push(document.querySelector(`#letter${i}`));
+const vowelBtn = document.querySelector('#vowel');
+const consonantBtn = document.querySelector('#consonant');
+const lettersForm = document.querySelector('#letters-form');
+const lettersInput = document.querySelector('#letters-input');
+const wordsEl = document.querySelector('#words');
+const lettersRestartBtn = document.querySelector('#letters-restart');
+
+//functions
+//====================================
+const connectFunction = socket => {
+	socket.on('connect', () => {
+		console.log(`You connected with id: ${socket.id}`);
+	});
+	socket.on('add-letter', addLetter);
+	socket.on('append-word', appendWord);
+	socket.on('clear-letters', clearLetters);
+	socket.emit('game-state', setGameState);
+};
+
+const addVowel = event => {
+	socket.emit('add-vowel');
+};
+
+const addConsonant = event => {
+	socket.emit('add-consonant');
+};
+
+const submitWord = event => {
+	event.preventDefault();
+	const word = lettersInput.value;
+	lettersInput.value = '';
+	socket.emit('submit-word', word);
+};
+
+const restartLetters = event => {
+	socket.emit('restart-letters');
+};
+
+const addLetter = (letter, index) => {
+	letterElArr[index].textContent = letter;
+};
+
+const appendWord = (word, score) => {
+	const wordEl = document.createElement('li');
+	wordEl.textContent = `${word} -> ${score} points`;
+	wordsEl.appendChild(wordEl);	
+};
+
+const clearLetters = () => {
+	for (let i=0; i<9; i++)
+		letterElArr[i].textContent = '';
+	wordsEl.innerHTML = '';
+	
+};
+
+const setGameState = (letters, words) => {
+	letters.forEach((letter, index) => addLetter(letter, index));
+	words.forEach(({ word, score }) => appendWord(word, score));
+};
+
+//listeners
+//=====================================
+vowelBtn.addEventListener('click', addVowel);
+consonantBtn.addEventListener('click', addConsonant);
+lettersForm.addEventListener('submit', submitWord);
+lettersRestartBtn.addEventListener('click', restartLetters);
+
+//body
+//=====================================
+let socket = io();	//front is same domain as server
+connectFunction(socket);
