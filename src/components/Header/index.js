0fa4700ab0f89742d21d99ad422b5bc84092a508
CWMasters <95546410+CWMasters@users.noreply.github.com>
2022-05-17 18:34:45 -0700

    page layout

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Header(props) {
+    const { currentPage, setCurrentPage } = props;
+
+return (
+    <header>L3tters</header>
+    );
+}
+
+
+
+
+
+export default Header;
