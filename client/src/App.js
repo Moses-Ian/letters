b5eb7c79f24471b55f093c3afc8c51231fd2c2ce
Hadasss <hadas.gadish@gmail.com>
2022-05-24 12:19:50 -0700

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import MainGame from "./components/MainGame";
+import Room from "./components/Room";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <Header />
+
+      {/* <MainGame room={room} /> */}
       <MainGame />
     </div>
   );
--- client/src/components/MainGame/index.jsx
+++ client/src/components/MainGame/index.jsx
@@ -1,129 +1,75 @@
 import React, { useState, useRef } from "react";
-import bricks from "../../assets/images/white-bricks.jpg";
 import "bulma/css/bulma.min.css";
+import { io } from "socket.io-client";
 
 const MainGame = () => {
+  // FYI FYI FYI
+  let room;
+
   //variables
   //==================================
   let letterElArr = [];
-
-  const letter = useRef();
   for (let i = 0; i < 9; i++)
     letterElArr.push(document.querySelector(`#letter${i}`));
-
   // const vowelBtn = document.querySelector("#vowel");
   // const consonantBtn = document.querySelector("#consonant");
   // const lettersForm = document.querySelector("#letters-form");
-  // FYI lettersInput was replaced with useRef:
   // const lettersInput = document.querySelector("#letters-input");
   const lettersInput = useRef();
   // const wordsEl = document.querySelector("#words");
   const wordsEl = useRef();
   // const lettersRestartBtn = document.querySelector("#letters-restart");
 
-  let lettersArr = [];
-  let vowelCount = 0;
-  let consonantCount = 0;
-
-  const vowels = ["a", "e", "i", "o", "u"];
-  const consonants = [
-    "d",
-    "h",
-    "t",
-    "n",
-    "s",
-    "p",
-    "y",
-    "f",
-    "g",
-    "c",
-    "r",
-    "l",
-    "q",
-    "j",
-    "k",
-    "x",
-    "b",
-    "m",
-    "w",
-    "v",
-    "z",
-  ];
-  const weights = [
-    4, 8, 12, 16, 20, 23, 26, 29, 31, 33, 35, 37, 38, 39, 40, 41, 42, 43, 44,
-    45, 46,
-  ];
-
   //functions
   //====================================
+  const connectFunction = (socket) => {
+    socket.on("connect", () => {
+      console.log(`You connected with id: ${socket.id}`);
+    });
+    socket.on("add-letter", addLetter);
+    socket.on("append-word", appendWord);
+    socket.on("clear-letters", clearLetters);
+    socket.on("set-game-state", setGameState);
+  };
+
   const addVowel = (event) => {
-    if (vowelCount === 5) return;
-    let vowel = vowels[Math.floor(Math.random() * 5)];
-    if (addLetter(vowel)) vowelCount++;
+    socket.emit("add-vowel", room);
   };
 
   const addConsonant = (event) => {
-    if (consonantCount === 6) return;
-    let random = Math.floor(Math.random() * weights[20]);
-    for (let i = 0; i < 21; i++) {
-      if (weights[i] > random) {
-        if (addLetter(consonants[i])) consonantCount++;
-        break;
-      }
-    }
-  };
-
-  const addLetter = (letter) => {
-    if (lettersArr.length === 9) return false;
-    letterElArr[lettersArr.length].textContent = letter.toUpperCase();
-    lettersArr.push(letter);
-    return true;
+    socket.emit("add-consonant", room);
   };
 
   const submitWord = (event) => {
     event.preventDefault();
     const word = lettersInput.value;
     lettersInput.value = "";
-    if (lettersArr.length !== 9) return;
-    const score = scoreWord(lettersArr, word);
-    // FYI
-    // const wordEl = document.createElement("li");
-    // wordEl.textContent = `${word} -> ${score} points`;
-    // converted to react:
-    const wordEl = React.createElement("li", {}, `${word} -> ${score} points`);
-    console.log(word);
-    wordsEl.appendChild(wordEl);
-  };
-
-  const scoreWord = (letters, word) => {
-    let checklist = new Array(word.length);
-    checklist.fill(false);
-
-    for (let i = 0; i < letters.length; i++)
-      for (let j = 0; j < word.length; j++)
-        if (letters[i] === word[j] && !checklist[j]) {
-          checklist[j] = true;
-          break;
-        }
-
-    for (let j = 0; j < checklist.length; j++) if (!checklist[j]) return 0;
-
-    if (!inDictionary(word)) return 0;
-
-    return word.length;
-  };
-
-  const inDictionary = (word) => {
-    //do something
-    return true;
+    socket.emit("submit-word", word, room);
   };
 
   const restartLetters = (event) => {
-    lettersArr = [];
-    vowelCount = 0;
-    consonantCount = 0;
+    socket.emit("restart-letters", room);
+  };
+
+  const addLetter = (letter, index) => {
+    letterElArr[index].textContent = letter.toUpperCase();
+  };
+
+  const appendWord = (word, score) => {
+    const wordEl = document.createElement("li");
+    wordEl.textContent = `${word} -> ${score} points`;
+    wordsEl.appendChild(wordEl);
+  };
+
+  const clearLetters = () => {
     for (let i = 0; i < 9; i++) letterElArr[i].textContent = "";
     wordsEl.innerHTML = "";
+  };
+
+  const setGameState = (letters, words) => {
+    clearLetters();
+    letters.forEach((letter, index) => addLetter(letter, index));
+    words.forEach(({ word, score }) => appendWord(word, score));
   };
 
   //listeners
@@ -133,21 +79,26 @@ const MainGame = () => {
   // lettersForm.addEventListener("submit", submitWord);
   // lettersRestartBtn.addEventListener("click", restartLetters);
 
+  //body
+  //=====================================
+  // let socket = io();	//front is same domain as server
+  let socket = io("http://localhost:3001"); //local
+  connectFunction(socket);
+
+  // joinRoom(getRoom());
+
   return (
     <div className="" id="letters-game">
-      {/* <img className="background" src={bricks} alt="bricks" /> */}
-
       <div className="rendered-letters" id="scramble">
-        <span ref={letter} id="letter0"></span>
-        <span ref={letter} id="letter1"></span>
-        <span ref={letter} id="letter2"></span>
-        <span ref={letter} id="letter3"></span>
-        <span ref={letter} id="letter4"></span>
-        <span ref={letter} id="letter5"></span>
-        <span ref={letter} id="letter6"></span>
-        <span ref={letter} id="letter7"></span>
-        <span ref={letter} id="letter8"></span>
-        {}
+        <span id="letter0"></span>
+        <span id="letter1"></span>
+        <span id="letter2"></span>
+        <span id="letter3"></span>
+        <span id="letter4"></span>
+        <span id="letter5"></span>
+        <span id="letter6"></span>
+        <span id="letter7"></span>
+        <span id="letter8"></span>
       </div>
 
       <div className="field m-3 is-flex">
@@ -188,7 +139,6 @@ const MainGame = () => {
                 value="Submit"
                 onClick={submitWord}
               />
-              {}
             </div>
           </div>
         </form>
