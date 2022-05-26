c1d94649a95caeae413435303a8b5bc5c1fcc72b
Hadasss <hadas.gadish@gmail.com>
2022-05-26 15:01:47 -0700

    added timer functionality

--- client/src/components/Timer/index.jsx
+++ client/src/components/Timer/index.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function Timer() {
-  return <div>Timer</div>;
+  const [counter, setCounter] = useState(30);
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (counter > 0) {
+        setCounter(counter - 1);
+      }
+      return;
+    }, 1000);
+  });
+
+  return <div>{counter}</div>;
 }
 
 export default Timer;
