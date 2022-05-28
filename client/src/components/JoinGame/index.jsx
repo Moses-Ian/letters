82d0cbceb955e11e75978eded6775151be48dd95
Hadasss <hadas.gadish@gmail.com>
2022-05-27 17:58:04 -0700

    consolidated join game bottons and moved to JoinGame component + added clear letters input

--- client/src/components/JoinGame/index.jsx
+++ client/src/components/JoinGame/index.jsx
@@ -1,31 +1,66 @@
 import React, { useState } from "react";
 import Modal2 from "../Modal-2";
+import MainGame from "../MainGame";
+import LiveChat from "../LiveChat";
 import "../../App.css";
 
-export default function JoinGame({ socket, username, room }) {
+export default function JoinGame({ socket, username }) {
   const [show, setShow] = useState(false);
+  const [room, setRoom] = useState("");
+
+  const joinRoom = (name) => {
+    socket.emit("join-game", name, room, (success, newRoom) => {
+      setRoom(newRoom);
+    });
+    console.log(`joinRoom ${name}`);
+  };
+
+  const joinRoomHandler = () => {
+    joinRoom("New Room");
+    setShow(false);
+  };
 
   return (
     <>
       <div className="column">
-        <button
-          className="join-game-button button is-warning"
-          onClick={() => setShow(true)}
-        >
-          {" "}
-          Join as {username}{" "}
-        </button>
+        {room == "" ? (
+          <button
+            className="join-game-button button is-warning"
+            onClick={() => setShow(true)}
+          >
+            Join as {username}
+          </button>
+        ) : (
+          ""
+        )}
       </div>
 
-      <Modal2 title="Join Game" onClose={() => setShow(false)} show={show}>
+      <Modal2
+        title="Join Game"
+        onClose={() => setShow(false)}
+        show={show}
+        joinRoomHandler={joinRoomHandler}
+      >
         <div>
           <p>
-            {" "}
             What is the name of the room you would like to join?
-            <input className="m-2" type="text" placeholder="Type room name" />
+            <input
+              className="input mt-2"
+              type="text"
+              placeholder="Type room name"
+            />
           </p>
         </div>
       </Modal2>
+
+      {room !== "" ? (
+        <>
+          <MainGame socket={socket} username={username} room={room} />
+          <LiveChat socket={socket} username={username} room={room} />
+        </>
+      ) : (
+        <p>You need to type a room name</p>
+      )}
     </>
   );
 }
--- client/src/components/Room/index.jsx
+++ client/src/components/Room/index.jsx
@@ -1,41 +1,13 @@
-import React, { useState } from "react";
-import MainGame from "../MainGame";
+import React from "react";
+
 import Header from "../Header";
-import LiveChat from "../LiveChat";
 
-function Room({ socket, username }) {
-  const [room, setRoom] = useState("");
-
-  const joinRoom = (name) => {
-    socket.emit("join-game", name, room, (success, newRoom) => {
-      setRoom(newRoom);
-    });
-    console.log(`joinRoom ${name}`);
-  };
-
-  const joinRoomHandler = () => {
-    joinRoom("New Room");
-  };
-
+function Room({ username }) {
   console.log("Room rendered");
 
   return (
     <div>
       <Header username={username} />
-      <button className="game-btn" onClick={joinRoomHandler}>
-        Join Game
-      </button>
-
-      <h1>You are playing in: {room}</h1>
-
-      {room !== "" ? (
-        <>
-          <MainGame socket={socket} username={username} room={room} />
-          <LiveChat socket={socket} username={username} room={room} />
-        </>
-      ) : (
-        <p>You need to type a room name</p>
-      )}
     </div>
   );
 }
