3edda8fd67f6169aa43ec1ad909170d944e64451
Hadasss <hadas.gadish@gmail.com>
2022-05-28 17:43:17 -0700

    timer centered

--- client/src/components/Timer/index.jsx
+++ client/src/components/Timer/index.jsx
@@ -17,7 +17,7 @@ function Timer() {
   };
 
   return (
-    <div className="timer-wrapper m-0">
+    <div className="timer-wrapper is-flex is-justify-content-center m-0">
       <CountdownCircleTimer
         isPlaying
         duration={30}
