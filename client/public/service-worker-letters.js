7ff52ca44ba26503479f1385a8528cafd3759677
CWMasters <95546410+CWMasters@users.noreply.github.com>
2022-06-08 16:22:16 -0700

    offline html background working

--- client/public/service-worker-letters.js
+++ client/public/service-worker-letters.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "version-1";
-const urlsToCache = [ 'index.html', 'offline.html' ];
+const CACHE_NAME = "version-02";
+const urlsToCache = [ 'index.html', 'offline.html', 'background5.cb5c31a8.jpg' ];
 const self = this;
 // Install SW
 self.addEventListener('install', (event) => {
@@ -9,15 +9,27 @@ self.addEventListener('install', (event) => {
                 console.log('Opened cache');
 return cache.addAll(urlsToCache);
             })
+            .catch(error => {
+                console.log(error)
+            })
     )
 });
 // Listen for requests
 self.addEventListener('fetch', (event) => {
+    console.log(event)
+    
+    const url = event.request.url
+    const split = url.split('/')
+    const fileName = split[split.length -1]
+    const name = fileName === '' ? 'offline.html':fileName
+    console.log(fileName)
     event.respondWith(
         caches.match(event.request)
             .then(() => {
                 return fetch(event.request) 
-                    .catch(() => caches.match('offline.html'))
+                    // .catch(() => caches.match('offline.html'))
+                    .catch(() => caches.match(name))
+                    
             })
     )
 });
