35289625029856631b2013f363154e4484c90a93
Hadasss <hadas.gadish@gmail.com>
2022-08-09 17:06:35 -0700

    App.css - changed colors to var names (where applicable)

--- server/server.js
+++ server/server.js
@@ -1,25 +1,25 @@
-const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const { authMiddleware } = require('./utils/auth');
-const { typeDefs, resolvers } = require('./schemas');
-const routes = require('./routes');
-const path = require('path');
-const helpers = require('./utils/helpers');
-const db = require('./config/connection');
-const registerHandlers = require('./handlers');
-require('dotenv').config({path: path.join(__dirname, '..', '.env')});
-const webPush = require('web-push');
-require('./utils/word-of-the-day');
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const { authMiddleware } = require("./utils/auth");
+const { typeDefs, resolvers } = require("./schemas");
+const routes = require("./routes");
+const path = require("path");
+const helpers = require("./utils/helpers");
+const db = require("./config/connection");
+const registerHandlers = require("./handlers");
+require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
+const webPush = require("web-push");
+require("./utils/word-of-the-day");
 
 // show memory usage
 // if (process.env.NODE_ENV === 'development') {
-	// setInterval(() => {
-		// console.log('===================================');
-		// for (const [key,value] of Object.entries(process.memoryUsage())){
-			// console.log(`Memory usage by ${key}, ${value/1000000}MB `)
-		// }
-		// }, 10000
-	// );
+// setInterval(() => {
+// console.log('===================================');
+// for (const [key,value] of Object.entries(process.memoryUsage())){
+// console.log(`Memory usage by ${key}, ${value/1000000}MB `)
+// }
+// }, 10000
+// );
 // }
 
 const app = express();
@@ -36,31 +36,35 @@ webPush.setVapidDetails(
 const apolloServer = new ApolloServer({
   typeDefs,
   resolvers,
-  context: authMiddleware
+  context: authMiddleware,
 });
 apolloServer.applyMiddleware({ app });
 
 //socket.io stuff
-const server = require('http').createServer(app);	
+const server = require("http").createServer(app);
 
 console.log(process.env.NODE_ENV);
-const coorsPolicy = process.env.NODE_ENV == 'development' ? 
-	{
-		cors: {
-			origin: ['http://localhost:3000']
-		}
-	} : {};
-const io = require('socket.io')(server, coorsPolicy)
-io.on('connection', (socket) => registerHandlers(io, socket));
+const coorsPolicy =
+  process.env.NODE_ENV == "development"
+    ? {
+        cors: {
+          origin: ["http://localhost:3000"],
+        },
+      }
+    : {};
+const io = require("socket.io")(server, coorsPolicy);
+io.on("connection", (socket) => registerHandlers(io, socket));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 app.use(routes);
 
-server.listen(PORT, () => { 
-	console.log(`Server running on port ${PORT}`); 
-	console.log(`Use GraphQL at http://localhost:${PORT}${apolloServer.graphqlPath}`);
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  console.log(
+    `Use GraphQL at http://localhost:${PORT}${apolloServer.graphqlPath}`
+  );
 });
