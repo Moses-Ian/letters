27d2ab4482ef691893db6e799d5590d460630851
Expired <imoses2@hotmail.com>
2022-08-11 12:58:25 -0700

    shareByPush done

--- client/src/components/Invite/index.jsx
+++ client/src/components/Invite/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Modal from "../Modal";
 import share from "../../assets/images/share.png";
 import { sanitize, validateEmail } from "../../utils";
@@ -20,13 +20,24 @@ export default function Invite () {
 
   const [show, setShow] = useState(false);
 	const [emailState, setEmailState] = useState("");
-	const [errorMsg, setErrorMsg] = useState("");
+	const [resultMsg, setResultMsg] = useState("");
+	const [checked, setChecked] = useState([]);
+	const [resultMsg2, setResultMsg2] = useState("");
 	
   const [shareLobbyByEmail] = useMutation(SHARE_LOBBY_BY_EMAIL);
   const [sendNotification] = useMutation(SEND_NOTIFICATION);
   const [getFriends, { loading, error: friendsError, data: friendsData }] =
     useLazyQuery(GET_FRIENDS); //data: friendsData takes data and puts it into friendsData
 
+	useEffect(() => {
+		if (show)
+			getFriends();
+	}, [show]);
+	
+	useEffect(() => {
+		if (show && friendsData)
+			setChecked(new Array(friendsData.me.friends.length).fill(false));
+	}, [show, friendsData]);
 
   const onShare = async () => {
     console.log("share");
@@ -73,26 +84,42 @@ export default function Invite () {
         },
       });
 			if (response.data.shareLobbyByEmail.success)
-				setErrorMsg("Invite sent.");
+				setResultMsg("Invite sent.");
 			else
-				setErrorMsg("Something went wrong.");
+				setResultMsg("Something went wrong.");
 			setEmailState("");
     } catch (err) {
       console.error(err);
-			setErrorMsg("Invalid email address.");
+			setResultMsg("Invalid email address.");
     }
   };
+	
+	const handleFriendsChange = event => {
+		const key = parseInt(event.target.dataset.key);
 
-  const shareByPush = (friends) => {
-    friends = ["ian", "moses", "ian2", "ianm"];
+		setChecked([
+			...checked.slice(0, key),
+			!checked[key],
+			...checked.slice(key+1)
+		]);
+	};
+	
+	const handleFriendsSubmit = event => {
+		event.preventDefault();
+		shareByPush();
+	}
 
+  const shareByPush = () => {
+    const friends = friendsData.me.friends.reduce((list, friend, index) => checked[index] ? [...list, friend.username] : list, [])
     try {
       sendNotification({ variables: { NotificationInput: { room, friends } } });
+			setResultMsg2("Invites sent");
     } catch (err) {
       console.error(err);
+			setResultMsg2("Something went wrong.");
     }
   };
-
+	
 	return (
 		<div>
 			<button className="lobby-btn" onClick={() => setShow(true)}>
@@ -115,14 +142,39 @@ export default function Invite () {
 							></input>
 							<button className="search-btn ml-1 button is-small is-warning">Send</button>
 						</div>
-						<div>{errorMsg}</div>
+						<div>{resultMsg}</div>
 					</div>
 				</form>
 				
 				{/* list of friends */}
-				
-					<p>Friends</p>
-				
+				<form onSubmit={handleFriendsSubmit}>
+					<p className="mt-2">Friends</p>
+					{friendsData ?
+						<div className="room-list m-2">
+							{friendsData.me.friends.map((friend, index) => (
+								<div className="room-list-div" key={index}>
+									<input 
+										className="room-list-radio"
+										type="checkbox"
+										name="friends"
+										data-key={index}
+										id={friend.username}
+										checked={checked[index] || false}
+										onChange={handleFriendsChange}
+									/>
+									<label className="room-list-label pl-2 pt-1 pb-1" htmlFor={friend.username}>
+										{friend.username}
+										<div className="room-list-label-border" />
+									</label>
+								</div>
+							))}
+						</div>
+					:
+						<p>You should add some friends.</p>
+					}
+					<button className="search-btn mt-2 ml-1 button is-small is-warning">Send</button>
+					<div>{resultMsg2}</div>
+				</form>
 				
 				{/* link to copy */}
 				<button className="l3tters-btn">Copy Link</button>
